c8744d5d7aafbbf4e64699efb53cd7ba3afc7f86
unknown <sheumbbaria@gmail.com>
2018-12-17 10:01:00 +0600

    java lab work

---
+++ README.md
@@ -0,0 +1,6 @@
+# JavaOperator
+All java operator are testing by Java unit test.
+Here Tested
+	1. Arithmetic Operator
+	2. Logical Operator
+	3. Bitwise Operator
---
+++ src/main/LogicalOperator.java
@@ -0,0 +1,16 @@
+package main;
+
+public class LogicalOperator {
+	public boolean or(boolean x,boolean y) {
+		return x|y;
+	}
+	public boolean and(boolean x,boolean y) {
+		return x&y;
+	}
+	public boolean not(boolean x) {
+		return !x;
+	}
+	public boolean xor(boolean x,boolean y) {
+		return x^y;
+	}
+}
